84613766e99c93bdbb5f7de4549f84a143772845
Ryan Son <ryan.son1002@gmail.com>
2023-02-08 21:29:40 +0900

    feat: 프로젝트 생성 및 gitignore 추가

---
+++ Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by Geonhee on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
