a05a19b07895a1ed71c894dc7209671f7d4d6325
Ryan Son <ryan.son1002@gmail.com>
2023-02-25 23:26:07 +0900

    feat: TCARecordMeeting 뼈대 작성

--- Standups/RecordMeeting/TCARecordMeeting.swift
+++ Standups/RecordMeeting/TCARecordMeeting.swift
@@ -5,16 +5,48 @@
 //  Created by Geonhee on 2023/02/26.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
+struct RecordMeetingFeature: ReducerProtocol {
+
+  struct State: Equatable {
+
+  }
+
+  enum Action: BindableAction {
+    case binding(BindingAction<State>)
+  }
+
+  var body: some ReducerProtocolOf<Self> {
+    BindingReducer()
+
+    Reduce<State, Action> { state, action in
+      switch action {
+      default:
+        return .none
+      }
+    }
+  }
+}
+
 struct TCARecordMeetingView: View {
+  let store: StoreOf<RecordMeetingFeature>
+
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    WithViewStore(self.store, observe: { $0 }) { viewStore in
+      Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
   }
 }
 
 struct RecordMeetingView_Previews: PreviewProvider {
   static var previews: some View {
-    TCARecordMeetingView()
+    TCARecordMeetingView(
+      store: Store(
+        initialState: RecordMeetingFeature.State(),
+        reducer: RecordMeetingFeature()
+      )
+    )
   }
 }
